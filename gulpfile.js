9b0c2e439af6b0fc0ace1c00f190e36cce05db3f
Martin Starosta <martin.starosta83@gmail.com>
2018-04-24 15:22:13 +0200

    Add tasks: clean, image to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,42 @@
 const autoprefixer = require('gulp-autoprefixer');
+const cache = require('gulp-cache');
+const clean = require('gulp-clean');
 const cssnano = require('gulp-cssnano');
 const gulp = require('gulp');
 const gutil = require('gulp-util');
+const imagemin = require('gulp-imagemin');
 const runSequence = require('run-sequence');
 const sass = require('gulp-sass');
 const zip = require('gulp-zip');
 
+/* Clean BUILD folder */
+gulp.task('clean', () => {
+  return gulp.src('./build', {
+      read: false
+    })
+    .pipe(clean());
+});
+
+/* Copy static (HTML, i18n, config.xml) files to BUILD folder */
 gulp.task('copy', () => {
   gulp.src(['./src/*.html', './src/i18n/*.*', './src/config.xml'])
   .pipe(gulp.dest('./build'))
 });
 
+/* Process images */
+gulp.task('images', function () {
+  return gulp.src('./src/images/**/*.+(png|jpg|gif|svg)')
+    .pipe(cache(imagemin()))
+    .pipe(gulp.dest('build/images'))
+});
+
+/* Process scripts - JavaScript files */
 gulp.task('scripts', () => {
   gulp.src('./src/scripts/*.js')
     .pipe(gulp.dest('./build/scripts'))
 });
 
+/* Process styles - SASS(CSS) files */
 gulp.task('styles', () => {
   gulp.src('./src/sass/*.scss')
     .pipe(sass({
@@ -30,20 +51,23 @@ gulp.task('styles', () => {
     .pipe(gulp.dest('./build/css'));
 });
 
+/* TODO: Create WGT archive for deploy in Orchestra */
 gulp.task('create-wgt', () => {
   gulp.src('build/*')
     .pipe(zip('staffButtonWidget.wgt'))
     .pipe(gulp.dest('widget'))
 });
 
+/* Build widget code */
 gulp.task('build', (cb) => {
   runSequence(
-    ['copy', 'scripts','styles'],
+    'clean', ['copy', 'scripts', 'styles', 'images'],
     'create-wgt',
     cb
   )
 });
 
+/* Watch for changes during development */
 gulp.task('watch-dev', () => {
   gulp.watch('./src/**', ['styles', 'copy', 'scripts']);
 })
